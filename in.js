d185f2bdd994f15ab56af39c14d3ace6df40d55e
Alibahri1 <bahriali00@gmail.com>
2025-05-24 14:44:15 +0300

    Add files via upload

---
+++ in.js
@@ -0,0 +1,65 @@
+ let isLarge = false;
+
+  function resizeImage() {
+    const img = document.getElementById("img1");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "400px";
+      img.style.height = "auto";
+    }
+    isLarge1 = !isLarge1;
+  }  function resizeImage2() {
+    const img = document.getElementById("img2");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "800px";
+      img.style.height = "auto";
+    }
+    isLarge1 = !isLarge1;
+  } 
+  
+  function resizeImage3() {
+    const img = document.getElementById("img3");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "400px";
+      img.style.height = "auto";
+    }
+    isLarge = !isLarge;
+  } function resizeImage4() {
+    const img = document.getElementById("img4");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "400px";
+      img.style.height = "auto";
+    }
+    isLarge = !isLarge;
+  } function resizeImage5() {
+    const img = document.getElementById("img5");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "400px";
+      img.style.height = "auto";
+    }
+    isLarge = !isLarge;
+  } function resizeImage6() {
+    const img = document.getElementById("img6");
+    if (isLarge) {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    } else {
+      img.style.width = "400px";
+      img.style.height = "auto";
+    }
+    isLarge = !isLarge;
+  }
